a0e76912ebed51c0e75863bdb8c5eaaa2ab61761
JoãoVictorSC <jv140280@gmail.com>
2024-03-14 16:29:34 -0300

    BUILD: Inicio do curso criando API RESTful com Node js

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import fastify from "fastify";
+import crypto from 'node:crypto'
+
+import { knex } from "./database";
+
+
+
+
+const app  = fastify()
+
+
+app.get('/hello', async () => {
+    const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+      
+        
+
+    return transactions
+}
+)
+
+app.listen({
+    port: 3333
+}).then(() => {
+    console.log('HTTP Server Running!')
+})
